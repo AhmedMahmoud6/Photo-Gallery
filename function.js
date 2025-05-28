1cbdbe8725ad4f1f8ebbc077c9d343faa3ec4ff6
Ahmed Mahmoud <ahmedmahmoudd626@gmail.com>
2025-05-28 17:59:47 +0300

    random photos

--- function.js
+++ function.js
@@ -1,24 +1,24 @@
-// const API_KEY = "jHzgUyOpNks1JyA5bYk8habW0pw8v27GL0ni69b7mNbTlx1vbFmeAf4p";
-// const query = "Nature";
+const API_KEY = "jHzgUyOpNks1JyA5bYk8habW0pw8v27GL0ni69b7mNbTlx1vbFmeAf4p";
+const page = Math.floor((Math.random() * 8000) / 6);
 
-// async function fetchPhotos() {
-//   try {
-//     const response = await fetch(
-//       `https://api.pexels.com/v1/search?query=${query}&per_page=1`,
-//       {
-//         headers: {
-//           Authorization: API_KEY,
-//         },
-//       }
-//     );
+async function fetchPhotos() {
+  try {
+    const response = await fetch(
+      `https://api.pexels.com/v1/curated?per_page=6&page=${page}`,
+      {
+        headers: {
+          Authorization: API_KEY,
+        },
+      }
+    );
 
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! status: ${response.status}`);
-//     }
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
 
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.error("Error fetching photos:", error);
-//   }
-// }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching photos:", error);
+  }
+}
--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-// fetchPhotos().then((data) => console.log(data));
+let photosContainer = document.querySelector(".photos-container");
+
+fetchPhotos().then((data) => {
+  for (let i of data.photos) {
+    let photoCardHtml = `
+        <div
+        class="photo-card lg:min-w-90 xl:max-w-105 h-72 rounded-lg overflow-hidden group transform transition-transform duration-500 hover:scale-105 relative cursor-pointer"
+      >
+        <img
+          class="w-full h-full object-cover transform transition-transform duration-500 group-hover:scale-110"
+          src="${i.src.landscape}"
+          alt=""
+        />
+        <div
+          class="photo-overlay flex items-end transform transition-transform duration-500 group-hover:translate-y-0 bg-gradient-to-b from-transparent to-black/70 h-30 absolute bottom-0 w-full p-12 pb-3 pt-10 px-6 translate-y-30"
+        >
+          <h3 class="text-white">${i.alt}</h3>
+        </div>
+      </div>
+        `;
+    photosContainer.insertAdjacentHTML("beforeend", photoCardHtml);
+  }
+});
