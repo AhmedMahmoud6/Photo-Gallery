435f06150c033da739cd593d52f505b53c245497
Ahmed Mahmoud <ahmedmahmoudd626@gmail.com>
2025-05-28 19:01:06 +0300

    display clicked photo

--- function.js
+++ function.js
@@ -1,5 +1,6 @@
 const API_KEY = "jHzgUyOpNks1JyA5bYk8habW0pw8v27GL0ni69b7mNbTlx1vbFmeAf4p";
 const page = Math.floor((Math.random() * 8000) / 6);
+let idCounter = 0;
 
 async function fetchPhotos() {
   try {
@@ -21,4 +22,60 @@ async function fetchPhotos() {
   } catch (error) {
     console.error("Error fetching photos:", error);
   }
+}
+
+function displayPhotos(data, photosContainer) {
+  for (let i of data.photos) {
+    idCounter += 1;
+    let photoCardHtml = `
+        <div
+        class="photo-card lg:min-w-90 xl:max-w-105 h-72 rounded-lg overflow-hidden group transform transition-transform duration-500 hover:scale-105 relative cursor-pointer"
+        id=${idCounter}
+      >
+        <img
+          class="w-full h-full object-cover transform transition-transform duration-500 group-hover:scale-110"
+          src="${i.src.landscape}"
+          alt=""
+        />
+        <div
+          class="photo-overlay flex items-end transform transition-transform duration-500 group-hover:translate-y-0 bg-gradient-to-b from-transparent to-black/70 h-30 absolute bottom-0 w-full p-12 pb-3 pt-10 px-6 translate-y-30"
+        >
+          <h3 class="text-white">${i.alt}</h3>
+        </div>
+      </div>
+        `;
+    photosContainer.insertAdjacentHTML("beforeend", photoCardHtml);
+  }
+}
+
+function displayClickedPhoto(clickedPhoto) {
+  let target = clickedPhoto.closest(".photo-card");
+  let targetId = target.id;
+  let imageHtml = `
+    <div
+      class="clicked-photo-container fixed top-0 bg-black/50 w-screen h-screen flex flex-col justify-center items-center gap-6 p-4 md:p-12"
+      id=${targetId}
+    >
+      <div class="close w-full flex justify-end">
+        <button
+          class="close-btn cursor-pointer w-12 h-12 rounded-full bg-[#1a1a1b] outline-3 outline-[#3d3d3d] text-white text-2xl flex justify-center items-center"
+        >
+          <i class="fa-solid fa-xmark text-white"></i>
+        </button>
+      </div>
+      <div
+        class="clicked-photo max-w-[90vw] max-h-[90vh] rounded-lg overflow-hidden"
+      >
+        <img
+          class="w-full h-full object-cover"
+          src="${target.querySelector("img").src}"
+          alt="clicked photo"
+        />
+      </div>
+      <div class="details text-center text-white text-lg md:text-2xl">
+      ${target.querySelector("h3").innerText}
+      </div>
+    </div>
+    `;
+  document.body.insertAdjacentHTML("beforeend", imageHtml);
 }
--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 let photosContainer = document.querySelector(".photos-container");
 
 fetchPhotos().then((data) => {
-  for (let i of data.photos) {
-    let photoCardHtml = `
-        <div
-        class="photo-card lg:min-w-90 xl:max-w-105 h-72 rounded-lg overflow-hidden group transform transition-transform duration-500 hover:scale-105 relative cursor-pointer"
-      >
-        <img
-          class="w-full h-full object-cover transform transition-transform duration-500 group-hover:scale-110"
-          src="${i.src.landscape}"
-          alt=""
-        />
-        <div
-          class="photo-overlay flex items-end transform transition-transform duration-500 group-hover:translate-y-0 bg-gradient-to-b from-transparent to-black/70 h-30 absolute bottom-0 w-full p-12 pb-3 pt-10 px-6 translate-y-30"
-        >
-          <h3 class="text-white">${i.alt}</h3>
-        </div>
-      </div>
-        `;
-    photosContainer.insertAdjacentHTML("beforeend", photoCardHtml);
+  displayPhotos(data, photosContainer);
+});
+
+document.addEventListener("click", (e) => {
+  // display photo when clicked
+  if (e.target.closest(".photo-card")) {
+    displayClickedPhoto(e.target);
+  }
+
+  console.log(e.target);
+
+  // close photo
+  if (
+    e.target.classList.contains("clicked-photo-container") ||
+    document.querySelector(".close").contains(e.target)
+  ) {
+    document.querySelector(".clicked-photo-container").remove();
   }
 });
